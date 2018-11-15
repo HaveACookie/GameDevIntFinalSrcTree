1589ee41cc13aef69b9087bbfac62c1df1a97671
Inno <williamchendev@gmail.com>
2018-11-15 14:52:22 -0500

    PlayerBehaviour Comments
    Added some comments!

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -6,20 +6,20 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour {
 
 	//Components
-	private GameManager gm;
-	private Rigidbody rb;
+	private GameManager gm;  //Game Manager Singleton
+	private Rigidbody rb;  //Player Physics Rigidbody
 	//private HealthPoints hp;
 	
 	//Settings
-	[SerializeField] private float spd;
-	[SerializeField] private float run_spd;
-	[SerializeField] private float turn_spd;
-	[SerializeField] private float back_spd;
+	[SerializeField] private float spd;  //Player Speed
+	[SerializeField] private float run_spd; //Player Running Speed
+	[SerializeField] private float turn_spd; //Player Turning Speed
+	[SerializeField] private float back_spd; //Player Moving Backwards Speed
 
-	private bool can_move;
+	private bool can_move;  //Turn Player Input on and off
 	
 	//Variables
-	private Vector2 velocity;
+	private Vector2 velocity;  //Player Physics movement in the x and z axis
 	
 	//Init Player
 	void Awake()
@@ -44,56 +44,57 @@ public class PlayerBehaviour : MonoBehaviour {
 	//Update Event
 	void Update () {
 		//Player Action
-		velocity = Vector2.zero;
+		velocity = Vector2.zero;  //Reset the player velocity to zero
 		if (can_move)
 		{
-			//Debug
+			//Debug (This block of code will be deleted later)
 			if (Input.GetKeyDown(KeyCode.K))
 			{
 				//pickupItem(30, 0, new GameObject("This is a test"));
-				gm.switchScene("map - Copy", "uwu");
+				gm.switchScene("MansionPartA", "door_b");
 			}
 
+			//Player Input
 			if (gm.getKey("aim"))
 			{
-				
+				//Attacking and Aiming Stances
 			}
 			else
 			{
 				//Variables
-				float angle_spd = 0f;
-				float move_spd = 0f;
+				float angle_spd = 0f;  //Change in angle that is applied to player's transform.eulerAngles.y
+				float move_spd = 0f;  //Movement Speed in the player's forward direction (If negative player moves backwards)
 
 				//Move forwards and backwards
 				float walk_run_spd = spd;
 				if (gm.getKey("run"))
 				{
-					walk_run_spd = run_spd;
+					walk_run_spd = run_spd;  //Set Player's Speed to Run Speed
 				}
 
 				if (gm.getKey("up"))
 				{
-					move_spd = walk_run_spd;
+					move_spd = walk_run_spd; //Move Forwards in the Run or Walk Speed
 				}
 				else if (gm.getKey("down"))
 				{
-					move_spd = -back_spd;
+					move_spd = -back_spd;  //Move Backwards in the Walk Backwards Speed
 				}
 
 				//Turning left and right
 				if (gm.getKey("left"))
 				{
-					angle_spd = -turn_spd;
+					angle_spd = -turn_spd;  //Turn Left
 				}
 				else if (gm.getKey("right"))
 				{
-					angle_spd = turn_spd;
+					angle_spd = turn_spd;  //Turn Right
 				}
 				
 				//Interact
 				if (gm.getKeyDown("interact"))
 				{
-					checkInteract();
+					checkInteract();  //Check for an object tagged with "Interact" with the checkInteract() method 
 				}
 				
 				//Set Movement Physics
@@ -105,6 +106,7 @@ public class PlayerBehaviour : MonoBehaviour {
 			//Inventory Menu
 			if (gm.getKey("inventory"))
 			{
+				//Create Inventory Canvas and turn off Player Movement
 				CameraManager.instance.createInventoryCanvas();
 				can_move = false;
 			}
@@ -114,6 +116,7 @@ public class PlayerBehaviour : MonoBehaviour {
 	//Physics
 	void FixedUpdate()
 	{
+		//Set Player's change in position with velocity
 		if (velocity != Vector2.zero)
 		{
 			rb.MovePosition(transform.position + new Vector3(velocity.x, 0, velocity.y));
@@ -122,7 +125,7 @@ public class PlayerBehaviour : MonoBehaviour {
 
 	void OnTriggerEnter(Collider collision)
 	{
-		//Check for Camera Trigger
+		//Check for Camera Trigger and set Camera associated with the Camera Trigger to be the active Camera
 		if (collision.gameObject.CompareTag("Finish"))
 		{
 			if (collision.GetComponent<CameraTrigger>() != null)
@@ -137,13 +140,14 @@ public class PlayerBehaviour : MonoBehaviour {
 	{
 		Collider[] hits;
 		
-		//Check Forwards
+		//Check for objects tagged with "Interact" in front of the Player
 		float facing_angle = (-transform.eulerAngles.y + 90) * Mathf.Deg2Rad;
 		Vector3 check_offset = new Vector3(Mathf.Cos(facing_angle), 0, Mathf.Sin(facing_angle)) * 0.8f;
 		hits = Physics.OverlapSphere(transform.position + check_offset, 0.5f);
 
 		foreach (Collider hit in hits)
 		{
+			//Perform inherited InteractInterface method action()
 			if (hit.gameObject.CompareTag("Interact"))
 			{
 				hit.GetComponent<InteractInterface>().action();
@@ -155,6 +159,7 @@ public class PlayerBehaviour : MonoBehaviour {
 	//Methods
 	public void pickupItem(int item, int stock, GameObject item_object)
 	{
+		//Opens up an Inventory Canvas and then sets the Inventory to show what item was picked up and freezes the Player
 		CameraManager.instance.createInventoryCanvas();
 		CameraManager.instance.setInventoryPickUp(item, stock, item_object);
 		can_move = false;
@@ -163,6 +168,8 @@ public class PlayerBehaviour : MonoBehaviour {
 	//Getter & Setters
 	public int equip
 	{
+		//Returns the Player's current equipped weapon
+		//If it returns -1, the player doesn't have anything equipped
 		get
 		{
 			if (gm.inventory.player_equip == -1)
@@ -175,6 +182,7 @@ public class PlayerBehaviour : MonoBehaviour {
 	
 	public int ammo
 	{
+		//Returns the player's current ammo count on their weapon
 		get
 		{
 			if (equip == -1)
@@ -191,6 +199,7 @@ public class PlayerBehaviour : MonoBehaviour {
 	
 	public bool canmove
 	{
+		//Sets the player's input to be frozen or not frozen
 		set
 		{
 			can_move = value;
@@ -200,6 +209,8 @@ public class PlayerBehaviour : MonoBehaviour {
 	//Debug
 	void OnDrawGizmos()
 	{
+		//Remove after done testing interact hit boxes
+		//Testing with hitboxes
 		Gizmos.color = Color.green;
 		Gizmos.DrawCube(transform.position - new Vector3(0, 0.8f, 0), new Vector3(2, 0.4f, 2));
 	}
