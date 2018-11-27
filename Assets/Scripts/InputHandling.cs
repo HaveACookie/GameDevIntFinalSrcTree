5e9e20f3d63fd3078b210ae9560dd2d566199add
Christian Weinschenk <cbw307@nyu.edu>
2018-11-27 00:37:39 -0500

    Pushing Some Stuff that wasnt working properly

--- Assets/Scripts/InputHandling.cs
+++ Assets/Scripts/InputHandling.cs
@@ -1,41 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//This is a script that is added to the player object to allow shooting to take place
 
-public class InputHandling : MonoBehaviour {
+
+public class InputHandling : MonoBehaviour
+{
 
     private RaycastHit hit;
+
+    public AudioSource pistolShotSource;
+    public AudioSource shotgunShotSource;
+    public AudioSource knifeShotSource;
+
+    //Surface Hit Bools
     public bool doorHit = false;
     public bool itemSurfaceHit = false;
     public bool shotHit = true;
-    public bool shooting = true;
+    //Variables for Which weapon is being used
+    public bool shooting = false;
+    public bool shootingPistol = false;
+    public bool isKnifing = false;
+    public bool isShotGunning = false;
+    public bool knifingSomeone = false; 
+
+
     public bool shootStance = true;
+    //Ray/LineCast Bools and Transform Holders
+    public bool shotgunShot1 = false;
+    public bool shotgunShot2 = false;
+    public bool shotgunShot3 = false;
+    public bool shotgunShot4 = false;
+    public Transform shootMidTopStart, shootMidTopEnd;
+    public Transform shootLeftMidEnd;
+    public Transform shootRightMidEnd;
+    public Transform shootDownMidEnd;
+
+
+
     // Use this for initialization
 
-
-
+    void shotgunCasting()
+    {
+        //LineCasting for shotgun, Debug line for visibility 
+        Debug.DrawLine(shootMidTopStart.position, shootMidTopEnd.position, Color.cyan);
+        shotgunShot1 = Physics.Linecast(shootMidTopStart.position, shootMidTopEnd.position, 1 << LayerMask.NameToLayer("Zombie"));
+        Debug.DrawLine(shootMidTopStart.position, shootLeftMidEnd.position, Color.magenta);
+        shotgunShot2 = Physics.Linecast(shootMidTopStart.position, shootLeftMidEnd.position, 1 << LayerMask.NameToLayer("Zombie"));
+        Debug.DrawLine(shootMidTopStart.position, shootRightMidEnd.position, Color.yellow);
+        shotgunShot3 = Physics.Linecast(shootMidTopStart.position, shootRightMidEnd.position, 1 << LayerMask.NameToLayer("Zombie"));
+        Debug.DrawLine(shootMidTopStart.position, shootDownMidEnd.position, Color.yellow);
+        shotgunShot4 = Physics.Linecast(shootMidTopStart.position, shootDownMidEnd.position, 1 << LayerMask.NameToLayer("Zombie"));
+    }
 
     // Update is called once per frame
     void Update()
     {
+        //Refrences to Raycasting Methods
         interactionCasting();
+        shotgunCasting();
         shotCasting();
         //Checks to see if the raycast is in range and if the player is pressing the desired button
         if (Input.GetKey(KeyCode.E) && doorHit.Equals(true))
         {
-            Debug.Log("weMadeit");
+            Debug.Log("DoorHitIsWorking");
             //Scene Management Stuff Involving Doors goes here
         }
 
-        if(Input.GetKey(KeyCode.E) && itemSurfaceHit.Equals(true))
+        if (Input.GetKey(KeyCode.E) && itemSurfaceHit.Equals(true))
         {
             //Stuff involving adding items to your inventory
         }
 
-    
-
-  
-
+        //Puts Player in Shootstance 
         if (Input.GetKey(KeyCode.LeftShift))
         {
             shootStance = true;
@@ -44,22 +81,55 @@ public class InputHandling : MonoBehaviour {
         {
             shootStance = false;
         }
-
-        if (shootStance = true && Input.GetKey(KeyCode.R))
+        if (shootStance == true && PlayerBehaviour.equip == 1 && Input.GetKeyDown(KeyCode.R))
         {
-            shooting = true;
+            knifingSomeone = true;
         }
 
-        if (shooting == true & shotHit.Equals(true))
+        if (knifingSomeone == true && isKnifing == true)
         {
-            Debug.Log("ShotWorked");
+            //Damage here
         }
-        
 
+        //Checks what weapon is being used by the inventory and shoots if its the pistol
+        if (shootStance == true && PlayerBehaviour.equip == 2 && Input.GetKeyDown(KeyCode.R))
+        {
+            shootingPistol = true;
+            //we can put the animations and sound effects around here later
+        }
+        if (shootingPistol == true && shotHit.Equals(true))
+        {
+            //Deal Damage here
+        }
+        //Checks what weapon is being used by the inventory and shoots if its the shotgun 
+        if (shootStance == true && PlayerBehaviour.equip == 3 && Input.GetKeyDown(KeyCode.R))
+        {
+            isShotGunning = true;
+            //we can put the animations and sound effects around here later
+        }
+        //Hit Checkers for each bullet in the spread
+        if (isShotGunning == true && shotgunShot1 == true)
+        {
+            //Should Deal Damage Here
+        }
+        if (isShotGunning == true && shotgunShot2 == true)
+        {
+            //Should Deal Damage Here
+        }
+        if (isShotGunning == true && shotgunShot3 == true)
+        {
+            //Should Deal Damage Here
+        }
+        if (isShotGunning == true && shotgunShot4 == true)
+        {
+            //Should Deal Damage Here
+        }
 
 
 
     }
+
+
 
 
     void shotCasting()
@@ -82,7 +152,18 @@ public class InputHandling : MonoBehaviour {
 
     }
 
-
+    void knifeCasting()
+    {
+        //Ask About spherecasting
+        //DefineSphere
+        RaycastHit sphereHit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        Physics.SphereCast(ray, 1.5f, out sphereHit, 2);
+        if (sphereHit.collider.tag.Equals("Enemy"))
+        {
+            isKnifing = true;
+        }
+    }
 
     void interactionCasting()
     {
@@ -114,6 +195,12 @@ public class InputHandling : MonoBehaviour {
 
                         break;
                     }
+                case "SaveSurface":
+                    {
+                        Debug.Log("SaveHit");
+                        // we can add a reference to saving management here
+                        break;
+                    }
                 default:
                     {
                         doorHit = false;
@@ -135,9 +222,9 @@ public class InputHandling : MonoBehaviour {
     }
 
 
-	
-	
 
 
-    
+
+
+
 }
