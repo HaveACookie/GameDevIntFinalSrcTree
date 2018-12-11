a3fe75abd67c83a0b3f7c59ba68bd3350d1e13a0
Inno <williamchendev@gmail.com>
2018-12-11 15:07:34 -0500

    Update 0.4.15
    Added Dog animation models

---
+++ Assets/Scripts/Enemies/EnemyAnimationScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimationScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
