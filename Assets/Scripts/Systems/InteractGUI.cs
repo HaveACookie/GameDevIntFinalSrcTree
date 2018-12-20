b5b11ffe7de5e538af003727d18e0f31b7b262bb
Fran <teddies@ymail.com>
2018-12-19 19:20:58 -0500

    merge

--- Assets/Scripts/Systems/InteractGUI.cs
+++ Assets/Scripts/Systems/InteractGUI.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI; 
 
-public class InteractGUI : MonoBehaviour {
+public class GUI : MonoBehaviour {
 
-    //usage: put this on gameobjects whihc need to be interacted with
+    //usage: put this on empty game UI object
     //purpose: prompt text on screen when interacting with things
 
 
-    /* don't destroy this object throughout the play scenes
+    // don't destroy this object throughout the play scenes
     void Awake()
     {
         GameObject[] objs = GameObject.FindGameObjectsWithTag("GUI");
@@ -17,7 +17,7 @@ public class InteractGUI : MonoBehaviour {
             Destroy(this.gameObject);
 
         DontDestroyOnLoad(this.gameObject);
-    }*/ 
+    }
 
     public Text GameUI;
 
