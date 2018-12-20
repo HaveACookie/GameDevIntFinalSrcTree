82f8356d75718a50ad515bc28391cfb9dc877bbe
Fran <teddies@ymail.com>
2018-12-20 16:53:43 -0500

    gameui updated a little

---
+++ Assets/Scripts/Interact/InteractGUI.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractGUI : MonoBehaviour
+{
+
+    //usage: put this on player
+    //purpose: prompt text on screen when interacting with things
+
+    public Text GameUI;
+    public Animator GUIAnimator; 
+
+    
+    void Start()
+    {
+        //supposed to find the component throughout all scenes, not working for some reason!!! 
+        Text GameUI = GameObject.Find("TextInGame").GetComponent<Text>(); //Text in the Canvas 
+        Animator GUIAnimator = GameObject.Find("TextInGame").GetComponent<Animator>(); //Animator for fade out 
+
+    }
+
+
+
+    private GameManager gm;  //Game Manager Singleton
+
+    //make the text fade in and out, plays after every text 
+    IEnumerator GUI() {
+        GUIAnimator.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(3f);
+        GUIAnimator.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(1f);
+        GameUI.text = " ";
+    }
+
+
+
+    //call all the different objects here with the text you want to go with it 
+    //trigger is on the player
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "LockedDoor")
+        {
+            GameUI.text = "Door is locked.";
+            StartCoroutine(GUI());
+
+        }
+
+        if (collision.gameObject.tag == "Stairs")   
+        {
+            GameUI.text = "Stairs seem a little unstable... Better not go up there.";
+            StartCoroutine(GUI());
+        }
+
+        if (collision.gameObject.tag == "Statue")
+        {
+            GameUI.text = "\"A woman drawing water\"";
+            StartCoroutine(GUI());
+        }
+    }
+}
---
+++ Assets/Scripts/noDestroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class noDestroy : MonoBehaviour {
+
+    //purpose: put this on the canvas within playscene
+    // don't destroy this object throughout the play scenes
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("GUI");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+
+      
+    }
+}
