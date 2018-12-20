70da137d10a7c1f788dacb5f658b4fee945f52f4
Fran <teddies@ymail.com>
2018-12-20 03:16:57 -0500

    GUI system added

--- Assets/Scripts/Systems/InteractGUI.cs
+++ Assets/Scripts/Systems/InteractGUI.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 
-public class GUI : MonoBehaviour {
+public class InteractGUI : MonoBehaviour
+{
 
-    //usage: put this on empty game UI object
+    //usage: put this on gameobjects whihc need to be interacted with
     //purpose: prompt text on screen when interacting with things
 
 
@@ -17,12 +18,37 @@ public class GUI : MonoBehaviour {
             Destroy(this.gameObject);
 
         DontDestroyOnLoad(this.gameObject);
+
+        //display nothing first
+        GameUI.text = " ";
     }
 
-    public Text GameUI;
 
-     void Update()
+    public Text GameUI; //Text in the Canvas 
+    public Animator GUIAnimator; //Animator for fade out 
+    private GameManager gm;  //Game Manager Singleton
+
+    //make the text fade out, plays after every text 
+    IEnumerator EndText() { 
+
+    yield return new WaitForSeconds(3f);
+        GUIAnimator.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(1f);
+    }
+
+    //call all the different objects here with the text you want to go with it 
+    void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.tag == "LockedDoor")
+        {
+            GameUI.text = "Door is locked.";
+            StartCoroutine(EndText());
+        }
+
+        if (collision.gameObject.tag == "Stairs")   
+        {
+            GameUI.text = "Stairs seem a little unstable... Better not go up there.";
+            StartCoroutine(EndText());
+        }
     }
 }
